d6bea2ba24eddf4121450ca8bad04fea37978f19
Gabrielalve1 <159019094+Gabrielalve1@users.noreply.github.com>
2026-09-13 22:56:40 -0300

    Prepara publicação do Meu Dinheiro via docs

---
+++ docs/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='meu-dinheiro-pages-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg','./icons/icon-180.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
