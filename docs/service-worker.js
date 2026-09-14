4184c163267ff14d759a70de9607eef82f4d8b9c
Gabrielalve1 <159019094+Gabrielalve1@users.noreply.github.com>
2026-09-13 23:10:10 -0300

    Atualiza cache da nova versão do app

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,1 @@
-const CACHE='meu-dinheiro-pages-v1';
-const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg','./icons/icon-180.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
+const CACHE='meu-dinheiro-v6';const FILES=['./','./index.html','./styles.css?v=6','./app.js?v=6','./manifest.webmanifest','./icon.svg','./icons/icon-180.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request)))});
